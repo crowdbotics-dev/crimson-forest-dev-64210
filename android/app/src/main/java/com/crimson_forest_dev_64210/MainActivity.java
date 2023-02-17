04585bbb1b8a3125289e104418b680297a34bb5a
Crowdbotics <engineering@crowdbotics.com>
2023-02-17 01:32:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/crimson_forest_dev_64210/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crimson_forest_dev_64210;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "crimson_forest_dev_64210";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-forest-dev-64210",
+  url: "https://crimson-forest-dev-64210.botics.co",
+  api: "https://crimson-forest-dev-64210.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
